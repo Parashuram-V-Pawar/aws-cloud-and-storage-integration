5a2e09fc3e82771e01d8795dcdab2f9698c479fe
Parashuram <parashurampawar56@gmail.com>
2026-03-02 17:27:53 +0530

    add file: [parashuram] added configuration file for 2nd project

---
+++ storage_and_data_engineering_with_rds/config/s3_client_config.py
@@ -0,0 +1,24 @@
+import os
+import boto3
+
+# AWS configuration
+AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
+BUCKET_NAME = os.getenv("BUCKET_NAME", "parashu-aws-lab-unique")
+
+# client initialization functions
+def get_s3_client():
+    session = boto3.Session(region_name=AWS_REGION)
+    return session.client("s3")
+
+def get_rds_client():
+    session = boto3.Session(region_name=AWS_REGION)
+    return session.client("rds")
+
+def get_ec2_client():
+    session = boto3.Session(region_name=AWS_REGION)
+    return session.client("ec2")
+
+# File paths
+LOCAL_UPLOAD_FILE = os.getenv("LOCAL_UPLOAD_FILE", "data/superstore_sales_6000_records.csv")
+S3_UPLOAD_KEY = os.getenv("S3_UPLOAD_KEY", "raw/superstore_sales_6000_records.csv")
+LOCAL_DOWNLOAD_FILE = os.getenv("LOCAL_DOWNLOAD_FILE", "download/superstore_sales_6000_records.csv")
