7433796a7dc5a040cd4f967d8c5edd787dea5e92
Parashuram <parashurampawar56@gmail.com>
2026-03-03 11:09:00 +0530

    modify file: [parashuram] updated upload file

--- storage_and_data_engineering_with_rds/scripts/upload_data.py
+++ storage_and_data_engineering_with_rds/scripts/upload_data.py
@@ -6,19 +6,19 @@ from config.s3_client_config import get_s3_client, BUCKET_NAME, LOCAL_UPLOAD_FIL
 
 logging.basicConfig(level=logging.INFO)
 
-def upload_file():
+def upload_file(local_file = LOCAL_UPLOAD_FILE, s3_key = S3_UPLOAD_KEY, bucket_name = BUCKET_NAME):
     s3 = get_s3_client()
 
-    if not os.path.exists(LOCAL_UPLOAD_FILE):
+    if not os.path.exists(local_file):
         logging.error("Local file does not exist.")
         return
     try:
         s3.upload_file(
-            LOCAL_UPLOAD_FILE,
-            BUCKET_NAME,
-            S3_UPLOAD_KEY
+            local_file,
+            bucket_name,
+            s3_key
         )
-        logging.info(f"Upload successful: s3://{BUCKET_NAME}/{S3_UPLOAD_KEY}")
+        logging.info(f"Upload successful: s3://{bucket_name}/{s3_key}")
     except FileNotFoundError:
         logging.error("The specified file was not found.")
     except NoCredentialsError:
@@ -26,16 +26,16 @@ def upload_file():
     except Exception as e:
         logging.error(f"Upload failed: {e}")
 
-def list_files_in_bucket():
+def list_files_in_bucket(bucket_name = BUCKET_NAME):
     s3 = get_s3_client()
     try:
-        response = s3.list_objects_v2(Bucket=BUCKET_NAME)
+        response = s3.list_objects_v2(Bucket = bucket_name)
         if 'Contents' in response:
-            logging.info(f"Files in bucket '{BUCKET_NAME}':")
+            logging.info(f"Files in bucket '{bucket_name}':")
             for obj in response['Contents']:
                 logging.info(f" - {obj['Key']}")
         else:
-            logging.info(f"No files found in bucket '{BUCKET_NAME}'.")
+            logging.info(f"No files found in bucket '{bucket_name}'.")
     except Exception as e:
         logging.error(f"Failed to list files: {e}")
         
