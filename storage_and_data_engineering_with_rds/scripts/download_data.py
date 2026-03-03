363f0f1277d7f1610eed7bdd828434cd832f65e1
Parashuram <parashurampawar56@gmail.com>
2026-03-03 11:09:39 +0530

    modify file: [parashuram] updated download file

--- storage_and_data_engineering_with_rds/scripts/download_data.py
+++ storage_and_data_engineering_with_rds/scripts/download_data.py
@@ -2,39 +2,39 @@
 import os
 import logging
 from botocore.exceptions import *
-from config.s3_client_config import *
+from config.s3_client_config import get_s3_client, BUCKET_NAME, LOCAL_DOWNLOAD_FILE, S3_UPLOAD_KEY
 
 logging.basicConfig(level=logging.INFO)
 
 # function to list files in S3 bucket
-def list_files_in_bucket():
+def list_files_in_bucket(bucket_name = BUCKET_NAME):
     s3 = get_s3_client()
     try:
-        response = s3.list_objects_v2(Bucket=BUCKET_NAME)
+        response = s3.list_objects_v2(Bucket = bucket_name)
         if 'Contents' in response:
-            logging.info(f"Files in bucket '{BUCKET_NAME}':")
+            logging.info(f"Files in bucket '{bucket_name}':")
             for obj in response['Contents']:
                 logging.info(f" - {obj['Key']}")
         else:
-            logging.info(f"No files found in bucket '{BUCKET_NAME}'.")
+            logging.info(f"No files found in bucket '{bucket_name}'.")
     except Exception as e:
         logging.error(f"Failed to list files: {e}")
 
 # Function to download a files from S3 bucket
-def download_file():
+def download_file(local_download_file = LOCAL_DOWNLOAD_FILE, s3_key = S3_UPLOAD_KEY, bucket_name = BUCKET_NAME):
     s3 = get_s3_client()
-    directory = os.path.dirname(LOCAL_DOWNLOAD_FILE)
+    directory = os.path.dirname(local_download_file)
 
     # Checks if download directory exists, if not creates it
     if directory: 
         os.makedirs(directory, exist_ok=True)
     try:
         s3.download_file(
-            BUCKET_NAME,
-            S3_UPLOAD_KEY,
-            LOCAL_DOWNLOAD_FILE
+            bucket_name,
+            s3_key,
+            local_download_file
         )
-        logging.info(f"Download successful: {LOCAL_DOWNLOAD_FILE}")
+        logging.info(f"Download successful: {local_download_file}")
         
     except ClientError as e:
         logging.error(f"S3 ClientError: {e}")
