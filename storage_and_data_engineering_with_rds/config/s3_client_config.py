2969a32462742b1e40dccb05822868c741b7407b
Parashuram <parashurampawar56@gmail.com>
2026-03-04 19:23:55 +0530

    add program: [parashuram] python script to initialize s3 client

--- storage_and_data_engineering_with_rds/config/s3_client_config.py
+++ storage_and_data_engineering_with_rds/config/s3_client_config.py
@@ -1,17 +1,29 @@
 import os
 import boto3
+from dotenv import load_dotenv
 
+# -----------------------------------------------------------------------
+# Load environment variables
+# -----------------------------------------------------------------------
+load_dotenv()
+
+# -----------------------------------------------------------------------
 # AWS configuration
-AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
-BUCKET_NAME = os.getenv("BUCKET_NAME", "parashu-aws-lab-unique")
+# -----------------------------------------------------------------------
+AWS_REGION = os.getenv("AWS_REGION")
+BUCKET_NAME = os.getenv("BUCKET_NAME")
 
+# -----------------------------------------------------------------------
 # client initialization functions
+# -----------------------------------------------------------------------
 def get_s3_client():
     session = boto3.Session(region_name=AWS_REGION)
     return session.client("s3")
 
+# -----------------------------------------------------------------------
 # File paths
-LOCAL_UPLOAD_FILE = os.getenv("LOCAL_UPLOAD_FILE", "data/superstore_sales.csv")
-S3_RAW_UPLOAD_KEY = os.getenv("S3_UPLOAD_KEY", "raw/superstore_sales.csv")
-LOCAL_DOWNLOAD_FILE = os.getenv("LOCAL_DOWNLOAD_FILE", "download/superstore_sales.csv")
-S3_PROCESSED_UPLOAD_KEY = os.getenv("S3_PROCESSED_UPLOAD_KEY", "processed/cleaned_superstore_sales.csv")
+# -----------------------------------------------------------------------
+LOCAL_UPLOAD_FILE = os.getenv("LOCAL_UPLOAD_FILE")
+S3_RAW_UPLOAD_KEY = os.getenv("S3_UPLOAD_KEY")
+LOCAL_DOWNLOAD_FILE = os.getenv("LOCAL_DOWNLOAD_FILE")
+S3_PROCESSED_UPLOAD_KEY = os.getenv("S3_PROCESSED_UPLOAD_KEY")
